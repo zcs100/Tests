d29f84da66bee025c14bfac65a69f311083474f1
sunzhichao <sunzhichao@lianjia.com>
2016-09-21 18:06:12 +0800

    init upload

---
+++ TestNIO/src/main/java/TestFirst.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+/**
+ * Created by szc on 16/9/21.
+ */
+
+public class TestFirst {
+
+    @Test
+    public void testFirst(){
+        System.out.print("=========first test class");
+    }
+}
