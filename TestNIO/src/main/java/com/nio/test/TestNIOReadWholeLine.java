f1429c85d2ed354195611548bf80e0b109bad356
sunzhichao <sunzhichao@lianjia.com>
2016-09-29 16:03:45 +0800

    add TestNIOReadWholeLine file

---
+++ TestNIO/src/main/java/com/nio/test/TestNIOReadWholeLine.java
@@ -0,0 +1,26 @@
+package com.nio.test;
+
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * Created by szc on 16/9/29.
+ * 读取完整的行,避免出现汉字乱码情况
+ */
+public class TestNIOReadWholeLine {
+
+    /**
+     * 乱码的情况
+     */
+    public void testDisorder() throws Exception{
+        RandomAccessFile file = new RandomAccessFile("/Users/szc/TestWorkPlace/Tests/TestNIO/src/main/java/testDisorder.txt","r");
+        FileChannel channel = file.getChannel();
+
+        ByteBuffer byteBuffer = ByteBuffer.allocate(256);
+        
+
+    }
+
+
+}
