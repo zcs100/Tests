8914810b54ea06b9ec9613e24cebee220ef36804
sunzhichao <sunzhichao@lianjia.com>
2016-10-08 11:02:39 +0800

    delete target clas

---
+++ TestSpring/src/main/java/com/quartz/source/TestQuartz.java
@@ -0,0 +1,15 @@
+package com.quartz.source;
+
+import org.junit.Test;
+
+/**
+ * Created by szc on 16/9/30.
+ */
+public class TestQuartz {
+
+    @Test
+    public void testQuartz(){
+        System.out.print("=======================");
+    }
+
+}
